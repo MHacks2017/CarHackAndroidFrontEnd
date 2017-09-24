67058ebf9aba666e629deca74fc73b0dfdcec4fc
MudkipMao <splatt9198@gmail.com>
2017-09-24 06:21:05 -0400

    Internet permission

---
+++ app/src/main/java/com/carhackers/carhack/TeslaAsynctask.java
@@ -0,0 +1,24 @@
+package com.carhackers.carhack;
+
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+/**
+ * Created by splat on 9/24/2017.
+ */
+
+public class TeslaAsynctask extends AsyncTask<Void, Void, String>
+{
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+
+        return null;
+    }
+}
